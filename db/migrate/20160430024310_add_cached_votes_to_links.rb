ee46ec17e900382eea0f52dc61dc4970da7a4b0d
wanqizhu <1213.ghs@gmail.com>
2016-04-30 18:29:36 -0500

    test migration

--- db/migrate/20160430024310_add_cached_votes_to_links.rb
+++ db/migrate/20160430024310_add_cached_votes_to_links.rb
@@ -1,5 +1,5 @@
 class AddCachedVotesToLinks < ActiveRecord::Migration
-  def change
+  def self.up
     add_column :links, :cached_votes_total, :integer, :default => 0
     add_column :links, :cached_votes_score, :integer, :default => 0
     add_column :links, :cached_votes_up, :integer, :default => 0
@@ -16,6 +16,16 @@ class AddCachedVotesToLinks < ActiveRecord::Migration
     add_index  :links, :cached_weighted_average
 
     # Uncomment this line to force caching of existing votes
-    Post.find_each(&:update_cached_votes)
+    # Post.find_each(&:update_cached_votes)
+  end
+
+  def self.down
+    remove_column :links, :cached_votes_total
+    remove_column :links, :cached_votes_score
+    remove_column :links, :cached_votes_up
+    remove_column :links, :cached_votes_down
+    remove_column :links, :cached_weighted_score
+    remove_column :links, :cached_weighted_total
+    remove_column :links, :cached_weighted_average
   end
 end
