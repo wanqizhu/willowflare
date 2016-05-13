4d63d41eb1b884cd98dbc4902f9e2bbbcbfaab14
wanqizhu <1213.ghs@gmail.com>
2016-05-13 17:22:11 -0500

    production assets config

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,4 +9,4 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += [ 'signUp.css', 'animations.css', 'links.css', 'navbar.css', 'comments.css', 'landingPage.css', 'editUser.css' ]
+# Rails.application.config.assets.precompile += [ 'signUp.css', 'animations.css', 'links.css', 'navbar.css', 'comments.css', 'landingPage.css', 'editUser.css' ]
