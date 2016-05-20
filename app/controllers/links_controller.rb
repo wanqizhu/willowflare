7b4defe44f24d13e8751016c8404a4a0bf914b27
Hui Zhu <huizhu1006@gmail.com>
2016-05-20 12:51:54 -0500

    Sort updates by time-updated, so newest posts go on top

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -15,7 +15,7 @@ class LinksController < ApplicationController
   # GET /links
   # GET /links.json
   def index
-    @links = Link.order(cached_weighted_score: :desc)
+    @links = Link.order(updated_at: :desc) # use cached_weighted_score for sorting by votes
   end
 
   # GET /links/1
