b84c11fc951a147a1164d76f009f257a266f2438
Hui Zhu <huizhu1006@gmail.com>
2016-05-20 09:28:32 -0500

    automated reward for completing user profile

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,6 +4,29 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
   def update_resource(resource, params)
+
+  	# reward for completing their profile
+  	if resource.info == nil or !(resource.info.include? "profile")
+	  	t = 1 # make sure they've filled everything
+	  	params.except(:password, :password_confirmation, :current_password).each do |p|
+	  		#puts p[1]
+	  		if p[1].blank?
+	  			t = 0
+	  		end
+	  	end
+
+	  	if t == 1
+	  		resource.money += 15
+	  		if resource.info == nil
+		        resource.info = 'profile_completed'
+		      else
+		        resource.info += ', profile_completed'
+	      	end
+	  	end
+	end
+
+
+
   	if params[:password].blank? && params[:password_confirmation].blank?
     	resource.update_without_password(params)
     else
