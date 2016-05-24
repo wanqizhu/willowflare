de1b0fbc28e8f955aef63533edf97ee97ec2f58f
wanqizhu <1213.ghs@gmail.com>
2016-05-24 14:49:23 -0500

    drop surveydata table if already exists

--- db/migrate/20160524011718_create_surveydata.rb
+++ db/migrate/20160524011718_create_surveydata.rb
@@ -1,5 +1,7 @@
 class CreateSurveydata < ActiveRecord::Migration
   def change
+  	drop_table(:surveydata, if_exists: true) 
+
     create_table :surveydata do |t|
       t.string :email
       t.text :surveyresponse
