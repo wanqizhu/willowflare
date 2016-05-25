c6edafabf6c7cc24b507906d42b91a2566b3c639
wanqizhu <1213.ghs@gmail.com>
2016-05-25 18:39:51 -0500

    test pw-reset email, auto-show profile if not completed

---
+++ app/models/surveydatum.rb
@@ -0,0 +1,49 @@
+class Surveydatum < ActiveRecord::Base
+
+	before_create :log_survey
+
+
+	def log_survey
+		if self.reward == nil or self.reward < 0
+			self.reward = 0
+		end
+
+		if self.email == nil
+			return
+		end
+
+		# This is pretty insecure lOL
+
+		if self.surveyresponse == nil or self.surveyresponse.length < 11 or self.surveyresponse[0, 10] != "SECR3T_KEY"
+			return
+		end
+
+
+		survey_num = self.surveyresponse[10]
+
+		u = User.where(email: self.email)[0]
+
+		# Check the user hasn't already completed the survey
+		if u != nil and (u.info == nil or !u.info.include?('survey'+survey_num))
+			if u.info == nil
+				u.info = 'surveydata_recorded_reward: ' + self.reward.to_s + '_at ' + Time.new.inspect + ', survey' + survey_num + '_completed'
+			else
+				u.info += ', surveydata_recorded_reward: ' + self.reward.to_s + '_at ' + Time.new.inspect + ', survey' + survey_num + '_completed'
+
+			end
+
+			u.money += self.reward
+			u.save
+
+
+			puts u.money
+			puts u.info
+			puts u.id
+		else
+			puts "NIL\n\n"
+			puts self.email + "\n\n"
+		end
+
+
+	end
+end
