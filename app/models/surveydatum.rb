2122afbc761cc83ee4a85299dad8a56ca221f2cf
wanqizhu <1213.ghs@gmail.com>
2016-05-26 20:09:08 -0500

    If an user completes a survey without an account, we add their email address to a text file so they get their rewards upon acc creation; modified so that all admins can edit reward-updates

--- app/models/surveydatum.rb
+++ app/models/surveydatum.rb
@@ -28,30 +28,37 @@ class Surveydatum < ActiveRecord::Base
 		u = User.where(email: self.email)[0]
 
 		# Check the user hasn't already completed the survey
-		if u != nil and (u.info == nil or !u.info.include?('survey'+survey_num))
-			if u.info == nil
-				u.info = 'surveydata_recorded_reward: ' + self.reward.to_s + '_at ' + Time.new.inspect + ', survey' + survey_num + '_completed'
+		if u != nil
+			if (u.info == nil or !u.info.include?('survey'+survey_num))
+				if u.info == nil
+					u.info = 'surveydata_recorded_reward: ' + self.reward.to_s + '_at ' + Time.new.inspect + ', survey' + survey_num + '_completed'
+				else
+					u.info += ', surveydata_recorded_reward: ' + self.reward.to_s + '_at ' + Time.new.inspect + ', survey' + survey_num + '_completed'
+
+				end
+
+				u.money += self.reward
+				if u.news != nil
+					u.news += ' Thanks for completing survey #' + survey_num + "! You've been rewarded with " + self.reward.to_s + "points.\n"
+				else
+					u.news = ' Thanks for completing survey #' + survey_num + "! You've been rewarded with " + self.reward.to_s + "points.\n"
+				end
+
+				u.save
+
+
+				puts u.money
+				puts u.info
+				puts u.id
 			else
-				u.info += ', surveydata_recorded_reward: ' + self.reward.to_s + '_at ' + Time.new.inspect + ', survey' + survey_num + '_completed'
-
+				puts "ALREADY_COMPLETED\n\n"
+				puts self.email + "\n\n"
+				puts self.surveyresponse + "\n\n"
 			end
-
-			u.money += self.reward
-			if u.news != nil
-				u.news += ' Thanks for completing survey #' + survey_num + "! You've been rewarded with " + self.reward.to_s + "points.\n"
-			else
-				u.news = ' Thanks for completing survey #' + survey_num + "! You've been rewarded with " + self.reward.to_s + "points.\n"
-			end
-
-			u.save
-
-
-			puts u.money
-			puts u.info
-			puts u.id
 		else
-			puts "NIL\n\n"
-			puts self.email + "\n\n"
+			File.open(Rails.root + ('config/survey00' + survey_num + '.txt'), 'a') { |f|
+				f.write(self.email + "\n")
+			}
 		end
 
 
