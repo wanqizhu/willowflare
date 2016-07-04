239b11395b372a16003a983666d61cc7ee446b01
wanqizhu <1213.ghs@gmail.com>
2016-07-04 11:27:42 -0500

    Automate store redemption by sending a notification email; need to move store item details server-side to add security

---
+++ app/mailers/my_mailer.rb
@@ -0,0 +1,13 @@
+class MyMailer < Devise::Mailer   
+  helper :application # gives access to all helpers defined within `application_helper`.
+  include Devise::Controllers::UrlHelpers # Optional. eg. `confirmation_url`
+  default template_path: 'devise/mailer' # to make sure that your mailer uses the devise views
+
+  default from: 'info@willowflare.com'
+
+  def store_redeem_email(user, item)
+  	@user = user
+  	@item = item
+  	mail(to: "info@willowflare.com", subject: "Store redemption by " + @user.email + " for " + @item)
+  end 
+end
