0557ee06dda156ecd80afd18c0165d3b1473818a
wanqizhu <1213.ghs@gmail.com>
2016-07-10 17:33:46 -0500

    basic user testing (not finished, need more work when time permits)

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert true
+  def setup
+  	@user = User.new(email: "test@test.com", password: "password")
+  	@user2 = User.new(email: "test2@test.com", password: "password", referral: "test@test.com")
+  	@user2 = User.new(email: "test2@test.com", password: "password", referral: "DO_NOT_EXIST")
+  
+  	@user.save
+  	@user2.save
+  end
+
+
+  test "user basics" do
+    assert @user.valid?
+    assert @user2.valid?
+    assert !@user3.valid?
+    
+    # use this to print to stdout (since we set config.logger = Logger.new(STDOUT) in test.rb)
+    Rails.application.config.logger.error User.where(username: "1")[0]
+    
+    assert @user.money == 50
+    assert @user2.money == 80, "check referral rewards work"
   end
 end
