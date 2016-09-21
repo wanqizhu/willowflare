f91a2a099602d9ad4789de0bba2592ff330ced01
wanqizhu <1213.ghs@gmail.com>
2016-09-20 19:41:18 -0500

    Integrated sign-up/sign-in buttons and forms on the landing page. To-do: make error msgs on signup show up dp directly on the form (maybe); MORE IMPORTANTLY, replace placeholder text and footer links.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,6 +18,26 @@ class ApplicationController < ActionController::Base
   before_filter :redirect_if_china, :except => [:companies]
 
 
+  # helpers to enable sign-up form on the landing page
+  helper_method :resource_name, :resource, :devise_mapping
+
+  def resource_name
+    :user
+  end
+ 
+  def resource
+    @resource ||= User.new
+  end
+ 
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
+
+
+
+
+
+
 
   def companies
     render :layout => false
