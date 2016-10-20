45d94399d3585ed681fe4cf08020671e30de134b
wanqizhu <1213.ghs@gmail.com>
2016-10-19 22:41:28 -0500

    Integrated accounts page, changed the default layouts/application.html.haml, will need to fix stylesheet linkings on other pages (and deal with application.js/css conflicts)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -46,8 +46,6 @@ class ApplicationController < ActionController::Base
   def landing_page
     if user_signed_in?
       redirect_to root_path
-    else
-      render :layout => "__application"
     end
   end
 
