e19efd4504835cb0a67754b8bb3d3da042b1f3e5
wanqizhu <1213.ghs@gmail.com>
2017-02-20 20:40:45 -0600

    Implemented file-uploading using CarrierWave - on new topics/posts, people can now upload images (yes, multiple :) ) and it'll be displayed along with the text - images are stored under Thredded::Post.last.images, in an array - so for example we can use Thredded::Post.last.images[0].url in an image-tag to display it - See CarrierWave Wiki for details
    - Thredded modifications:
    	- display upload-image option on new-topic/post-form (Views)
    	- display uploaded images for posts (Views)
    	- permit {images: []} as a parameter for topic/post (Controller)
    	- mount uploaders for topic/post (Model)

--- app/models/thredded/post.rb
+++ app/models/thredded/post.rb
@@ -2,6 +2,12 @@ require_dependency File.expand_path('../../app/models/thredded/post', Thredded::
 
 module Thredded
   class Post # reopen
+
+  	# # carrier_wave uploader
+  	mount_uploaders :images, ImagesUploader
+  	serialize :images, Array # make it into an array (equiv. to JSON, but sqlite3 doesn't support JSON types
+  	# so we can upload multiple images
+
     def autofollow_and_notify
       #no-op
     end
