4eb47b2ca3e7d17aef34eb986f8dc0bc84461633
wanqizhu <1213.ghs@gmail.com>
2017-02-28 15:18:23 -0600

    No longer raise mailer errors in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false  # true -- in case we exceed daily quota
   config.action_mailer.default_url_options = { :host => 'willowflare.com' }
   Rails.application.routes.default_url_options[:host] = 'willowflare.com'
 
