0a90039104c9aac87417959724306d738f5b9bf7
wanqizhu <1213.ghs@gmail.com>
2017-03-02 10:59:10 -0600

    Fixed small NIL bug in env['HTTP_USER_AGENT']; reorganized readme-and-other-dev files

---
+++ dev_readmes/example_queries.sql
@@ -0,0 +1,13 @@
+-- UPDATE users SET money = money + 30, 
+-- 	info = CONCAT(info, ', manually entering 30 pts for bugreport #5 - 3/2/17'), 
+-- 	news = CONCAT(news, "\nThanks for reporting bug #5. We have awarded you 30 points")
+--     WHERE id=6142; # give user id=6142 money, and log it in the user's /info/ field
+    # make sure to run the same query with SELECT first to check which user you're selecting
+
+-- SELECT * from users WHERE email = "test@test.com";
+-- UPDATE users SET money = 0 WHERE email = "test@test.com";
+
+-- SELECT id, email, money, info, news from users WHERE email = "test@test.com"; # only get needed fields
+-- SELECT * from users WHERE info LIKE "%survey11%"; # get all users whose info include the string "survey11" -- % means wildcard
+-- SELECT * from users WHERE email LIKE "%@willowflare.com"; # get all emails that end with @willowflare.com
+-- 
